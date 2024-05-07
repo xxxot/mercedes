eb8361af2cbed747bfe999a4efda6ac9f044fd78
xxxot <134232764+xxxot@users.noreply.github.com>
2024-05-07 07:55:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var f = ['Agnieszka', 'Marcela', 'Paulina', 'Gosia'];
-  var m = ['Rafał', 'Darek', 'Szymon'];
+  var m = ['Rafał', 'Darek', 'Kamil'];
 
   const COLUMNS = 4;
   var ludzie = m.concat(f);
